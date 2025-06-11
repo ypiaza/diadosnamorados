25e4fb7859fca8e2fc9f58d859234ccdcc677863
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-06-11 18:38:30 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import background from '../public/15375094-uhd_3840_2160_30fps.mp4'
+import background from '../public/pexels-iriser-1122639.jpg'
 import nos1 from '../public/IMG_1637.jpg'
 import nos2 from '../public/IMG-20230302-WA0069.jpg'
 import nos3 from '../public/IMG_4149.jpg'
@@ -10,36 +10,37 @@ const App = () => {
   const [click, setClick] = useState(false)
 
   return (
-    <div className='relative flex items-center justify-center w-full h-screen overflow-hidden px-8 py-20'>
-      <video autoPlay muted loop className="absolute inset-0 w-full h-full object-cover -z-10">
-        <source src={background} type="video/mp4" />
-      </video>
-      <div className='flex items-center flex-col justify-evenly w-full h-full bg-white/30 backdrop-blur border border-white/20 rounded-2xl px-8 transition-all '>
-        <div className={`flex-col gap-2 ${click ? 'hidden' : 'flex'}`}>
-          <img src={nos1} className='rounded-md border border-white/20' />
-          <img src={nos2} className='rounded-md border border-white/20' />
-          <img src={nos3} className='rounded-md border border-white/20' />
+    <div className='relative flex items-center justify-center w-full h-screen overflow-hidden px-2 py-32 bg-center bg-cover bg-no-repeat'
+         style={{
+          backgroundImage:`url(${background})`
+         }}>
+      <div className='flex items-center flex-col justify-evenly w-full h-full bg-transparent backdrop-blur border-2 border-t-white/30 border-r-white/30 border-b-white/20 border-l-white/10 rounded-2xl px-8 transition-all '>
+        <div className={`grid-cols-2 gap-2 ${click ? 'hidden' : 'grid'}`}>
+          <img src={nos1} className='rounded-md border object-cover border-white/20' />
+          <img src={nos2} className='rounded-md border object-cover border-white/20' />
+          <img src={nos3} className='rounded-md border object-cover border-white/20' />
+          <img src={nos1} className='rounded-md border object-cover border-white/20' />
+          <img src={nos2} className='rounded-md border object-cover border-white/20' />
+          <img src={nos3} className='rounded-md border object-cover border-white/20' />
         </div>
-        <div className={`flex-col justify-between gap-5 ${click === false ? 'hidden' : 'flex'}`}>
+        <div className={`flex-col justify-evenly gap-2 p-5 ${click === false ? 'hidden' : 'flex'}`}>
           <div className={'flex item-center justify-between'} >
-            <img src={nos1} className='rounded-md border border-white/20' />
+            <img src={nos1} className='rounded-xl border object-cover border-white/20' />
           </div>
           <div>
-            <h1 className='text-4xl font-bold text-red-700/60'>Oi, gatinha!</h1>
-            <p className='text-xl font-medium text-black/80 text-justify'>Fiz isso aqui só para te lembrar nesse dia dos namorados, que você é muito, muito, muito especial. Eu sou muito feliz de ter você todos os dias comigo, você é a prova do amor e cuidado de Deus para mim e é por você que não desisto e quero cada dia ser melhor. Te amo e te quero para sempre e sempre!</p>
+            <h1 className='text-4xl font-bold text-red-600'>Oi, gatinha!</h1>
+            <p className='text-xl font-medium text-white/80 text-justify'>Fiz isso aqui só para te lembrar nesse dia dos namorados, que você é muito, muito, muito especial. Eu sou muito feliz de ter você todos os dias comigo, você é a prova do amor e cuidado de Deus para mim e é por você que não desisto e quero cada dia ser melhor. Te amo e te quero para sempre e sempre!</p>
           </div>
           <div className='flex items-center justify-center gap-5 transition-all'>
-            <FaHeart className='text-red-500 text-3xl animate-bounce delay-75' />
-            <FaHeart className='text-red-500 text-3xl animate-bounce delay-100'/>
-            <FaHeart className='text-red-500 text-3xl animate-bounce delay-75'/>
-            <FaHeart className='text-red-500 text-3xl animate-bounce delay-100'/>
-            <FaHeart className='text-red-500 text-3xl animate-bounce delay-75'/>
-            <FaHeart className='text-red-500 text-3xl animate-bounce delay-100'/>
+            
           </div>
         </div>
-        <button onClick={() => setClick(true)} className={`bg-red-700/60 shadow-2xl shadow-blue-500/50 text-white text-2xl font-bold p-5 rounded-2xl ${click ? 'hidden' : ''}`}>Clique aqui!</button>
-        <button onClick={() => setClick(false)} className={`bg-red-700/60 shadow-2xl shadow-red-500/50 text-white text-2xl font-bold p-5 rounded-2xl ${click ? '' : 'hidden'}`}>
-TE AMO!</button>
+        <button onClick={() => setClick(true)} className={`bg-red-700/60 shadow-2xl shadow-red-500/50 text-white text-2xl font-bold p-5 rounded-2xl ${click ? 'hidden' : ''}`}>Clique aqui!</button>
+        <button onClick={() => setClick(false)} className={`flex items-center justify-center gap-3 bg-red-700/60 shadow-2xl shadow-red-500/50 text-white text-2xl font-bold p-5 rounded-2xl ${click ? '' : 'hidden'}`}>
+TE AMO!
+            <FaHeart className='text-red-500 text-3xl animate-bounce delay-75' />
+
+</button>
       </div>
     </div>
   )
