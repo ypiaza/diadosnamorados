2a13e746c0e0acb65a49211e18108f7a31869475
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-06-11 14:38:22 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -21,16 +21,14 @@ const App = () => {
           <img src={nos3} className='rounded-md border border-white/20' />
         </div>
         <div className={`flex-col justify-evenly gap-5 ${click === false ? 'hidden' : 'flex'}`}>
-          <div className='flex item-center justify-between w-full'}>
+          <div className={'flex item-center justify-between'} >
             <img src={nos1} className='rounded-md border border-white/20' />
-            <img src={nos2} className='rounded-md border border-white/20' />
-            <img src={nos3} className='rounded-md border border-white/20' />
           </div>
           <div>
             <h1 className='text-4xl font-bold text-red-700/60'>Oi, gatinha!</h1>
             <p className='text-xl font-medium text-black/80 text-justify'>Fiz isso aqui só para te lembrar nesse dia dos namorados, que você é muito, muito, muito especial. Eu sou muito feliz de ter você todos os dias comigo, você é a prova do amor e cuidado de Deus para mim e é por você que não desisto e quero cada dia ser melhor. Te amo e te quero para sempre e sempre!</p>
           </div>
-          <div className='flex items-center justify-center mt-12 gap-5 transition-all'>
+          <div className='flex items-center justify-center gap-5 transition-all'>
             <FaHeart className='text-red-500 text-3xl animate-bounce delay-75' />
             <FaHeart className='text-red-500 text-3xl animate-bounce delay-100'/>
             <FaHeart className='text-red-500 text-3xl animate-bounce delay-75'/>
@@ -39,7 +37,7 @@ const App = () => {
             <FaHeart className='text-red-500 text-3xl animate-bounce delay-100'/>
           </div>
         </div>
-        <button onClick={() => setClick(true)} className={`bg-blue-700/60 shadow-2xl shadow-blue-500/50 text-white text-2xl font-bold p-5 rounded-2xl ${click ? 'hidden' : ''}`}>Clique aqui!</button>
+        <button onClick={() => setClick(true)} className={`bg-red-700/60 shadow-2xl shadow-blue-500/50 text-white text-2xl font-bold p-5 rounded-2xl ${click ? 'hidden' : ''}`}>Clique aqui!</button>
         <button onClick={() => setClick(false)} className={`bg-red-700/60 shadow-2xl shadow-red-500/50 text-white text-2xl font-bold p-5 rounded-2xl ${click ? '' : 'hidden'}`}>
 TE AMO!</button>
       </div>
