fb32ec1fe1a600320a625fc0b8f31181cc8f142f
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-06-11 23:49:03 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
   const [click, setClick] = useState(false)
 
   return (
-    <div className='flex items-center justify-center w-full h-screen overflow-hidden px-2 py-26 bg-center bg-cover bg-no-repeat'
+    <div className='flex items-center justify-center w-full h-screen overflow-hidden px-2 py-6 bg-center bg-cover bg-no-repeat'
          style={{
           backgroundImage:`url(${background})`
          }}>
@@ -28,17 +28,14 @@ const App = () => {
             <img src={nos1} className='rounded-xl border object-cover border-white/20' />
           </div>
           <div>
-            <h1 className='text-4xl font-bold text-red-600'>Oi, gatinha!</h1>
-            <p className='text-xl font-medium text-white/80 text-justify'>Fiz isso aqui só para te lembrar nesse dia dos namorados, que você é muito, muito, muito especial. Eu sou muito feliz de ter você todos os dias comigo, você é a prova do amor e cuidado de Deus para mim e é por você que não desisto e quero cada dia ser melhor. Te amo e te quero para sempre e sempre!</p>
-          </div>
-          <div className='flex items-center justify-center gap-5 transition-all'>
-            
+            <h1 className='text-2xl font-bold text-red-600'>Oi, gatinha!</h1>
+            <p className='font-medium text-white/80 text-justify'>Fiz isso aqui só para te lembrar nesse dia dos namorados, que você é muito, muito, muito especial. Eu sou muito feliz de ter você todos os dias comigo, você é a prova do amor e cuidado de Deus para mim e é por você que não desisto e quero cada dia ser melhor. Te amo e te quero para sempre e sempre!</p>
           </div>
         </div>
-        <button onClick={() => setClick(true)} className={`bg-red-700/60 shadow-2xl shadow-red-500/50 text-white text-2xl font-bold p-5 rounded-2xl ${click ? 'hidden' : ''}`}>Clique aqui!</button>
-        <button onClick={() => setClick(false)} className={`flex items-center justify-center gap-3 bg-red-700/60 shadow-2xl shadow-red-500/50 text-white text-2xl font-bold p-5 rounded-2xl ${click ? '' : 'hidden'}`}>
+        <button onClick={() => setClick(true)} className={`bg-red-700/60 shadow-2xl shadow-red-500/50 text-white font-bold p-5 rounded-2xl ${click ? 'hidden' : ''}`}>Clique aqui!</button>
+        <button onClick={() => setClick(false)} className={`flex items-center justify-center gap-3 bg-red-700/60 shadow-2xl shadow-red-500/50 text-white font-bold p-5 w-full rounded-2xl ${click ? '' : 'hidden'}`}>
 TE AMO!
-            <FaHeart className='text-red-500 text-3xl animate-bounce delay-75' />
+            <FaHeart className='text-red-500 text-xl animate-bounce delay-75' />
 
 </button>
       </div>
